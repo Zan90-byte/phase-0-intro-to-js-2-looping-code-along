2ba3a70c8571c7547ad839f975b4b4e37739f9d3
Jean Raphael Barbosa Santos <jrbasan@hotmail.com>
2024-09-28 15:34:55 -0700

    done looping lab

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Code your solutions in this file
+
+const messages = [];
+
+function writeCards(names, event) {
+    for (let i = 0; i < names.length; i++) {
+        const thankYou = `Thank you, ${names[i]}, for the wonderful ${event} gift!`;
+        messages.push(thankYou);
+    }
+    return messages;
+}
+
+//console.log(writeCards(["Charlie", "Samip", "Ali"], "birthday"));
+
+//const number = 10;
+
+
+function countDown(number) {
+    let i = 0;
+    while (i <= number) {
+        console.log(i++);
+    }
+    return number;
+}
+//console.log(countDown(number));
